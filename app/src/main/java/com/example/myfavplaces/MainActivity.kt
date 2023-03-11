969eea630296451c39224e687a50d2b67c396be6
Abhay Singh <singhchirag188@gmail.com>
2023-03-11 22:25:22 +0530

    Now the data is preserved even after closing the app. And app is finally completely ready.

--- app/src/main/java/com/example/myfavplaces/MainActivity.kt
+++ app/src/main/java/com/example/myfavplaces/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myfavplaces
 
 import android.app.Activity
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
@@ -16,6 +17,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutput
+import java.io.ObjectOutputStream
 
 data class Place(val title:String,val description:String,val latitude:Double,val longitude:Double):java.io.Serializable
 
@@ -25,6 +32,7 @@ public val MAPS_EXTRA = "com.example.myfavplaces.MAPS_EXTRADATA"
 private val TAG="MainActivity"
 private val REQUEST_CODE=1234
 public val MAP_TITLE = "com.example.myfavplaces.MAP_TITLE"
+private const val FILE_NAME = "com.example.myfavplaces.userplace.data"
 
 private var datasource = mutableListOf<userplace>()
 lateinit var mapAdapter:Maps_custom_adapter
@@ -36,103 +44,86 @@ class MainActivity : AppCompatActivity() {
 
         val rvid = findViewById<RecyclerView>(R.id.rvid)
 
-        datasource = generateSampleData().toMutableList()
+
+        //assign data source from reading from the file. by using deserialize and convert file to List<userplace>
+        datasource = deserializeUserPlaces(this).toMutableList()
 
         rvid.layoutManager = LinearLayoutManager(this)
 
-        mapAdapter = Maps_custom_adapter(this,datasource,object:Maps_custom_adapter.onClickListener{
-            override fun onClick(position:Int) {
-                val intent = Intent(this@MainActivity,DisplayMapsActivity::class.java)
-                intent.putExtra(MAPS_EXTRA,datasource[position])
-                startActivity(intent)
-            }
-        })
+        mapAdapter =
+            Maps_custom_adapter(this, datasource, object : Maps_custom_adapter.onClickListener {
+                override fun onClick(position: Int) {
+                    val intent = Intent(this@MainActivity, DisplayMapsActivity::class.java)
+                    intent.putExtra(MAPS_EXTRA, datasource[position])
+                    startActivity(intent)
+                }
+            })
 
-        rvid.adapter= mapAdapter
+        rvid.adapter = mapAdapter
 
         //when user clicked we want to move to new activity
         val fav_add_button = findViewById<FloatingActionButton>(R.id.fav_add_button)
         fav_add_button.setOnClickListener {
-            Log.i(TAG,"Action Button Clicked!")
+            Log.i(TAG, "Action Button Clicked!")
             favButtonTask()
         }
     }
 
-    private fun favButtonTask(){
-        val view = LayoutInflater.from(this).inflate(R.layout.dailogue_add_title,null)
+    private fun favButtonTask() {
+        val view = LayoutInflater.from(this).inflate(R.layout.dailogue_add_title, null)
         val dailogue = AlertDialog.Builder(this)
             .setTitle("Create a Title")
             .setView(view)
-            .setPositiveButton("OK",null)
-            .setNegativeButton("Cancel",null)
+            .setPositiveButton("OK", null)
+            .setNegativeButton("Cancel", null)
             .show()
 
         dailogue.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
-            val titletext:String = view.findViewById<EditText>(R.id.titleid).text.trim().toString()
-            if(titletext==""){
+            val titletext: String = view.findViewById<EditText>(R.id.titleid).text.trim().toString()
+            if (titletext == "") {
                 Toast.makeText(this, "Please fill the title to proceed", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             dailogue.dismiss()
-            val intent = Intent(this@MainActivity,CreateMapsActivity::class.java)
+            val intent = Intent(this@MainActivity, CreateMapsActivity::class.java)
 
-            intent.putExtra(MAP_TITLE,titletext)
-            startActivityForResult(intent,REQUEST_CODE)
+            intent.putExtra(MAP_TITLE, titletext)
+            startActivityForResult(intent, REQUEST_CODE)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode== REQUEST_CODE && resultCode== Activity.RESULT_OK){
-            Log.i(TAG,"Got data back from user")
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Log.i(TAG, "Got data back from user")
             val cuserplace = data?.getSerializableExtra(MAPS_EXTRA) as userplace
             datasource.add(cuserplace)
-            mapAdapter.notifyItemInserted(datasource.size-1)
+            mapAdapter.notifyItemInserted(datasource.size - 1)
+
+            //as a new item is inserted update it into the local file
+            serializeUserPlaces(this, datasource)
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
 
-    private fun generateSampleData(): List<userplace> {
-        return listOf(
-            userplace(
-                "Memories from University",
-                listOf(
-                    Place("Branner Hall", "Best dorm at Stanford", 37.426, -122.163),
-                    Place("Gates CS building", "Many long nights in this basement", 37.430, -122.173),
-                    Place("Pinkberry", "First date with my wife", 37.444, -122.170)
-                )
-            ),
-            userplace("January vacation planning!",
-                listOf(
-                    Place("Tokyo", "Overnight layover", 35.67, 139.65),
-                    Place("Ranchi", "Family visit + wedding!", 23.34, 85.31),
-                    Place("Singapore", "Inspired by \"Crazy Rich Asians\"", 1.35, 103.82)
-                )),
-            userplace("Singapore travel itinerary",
-                listOf(
-                    Place("Gardens by the Bay", "Amazing urban nature park", 1.282, 103.864),
-                    Place("Jurong Bird Park", "Family-friendly park with many varieties of birds", 1.319, 103.706),
-                    Place("Sentosa", "Island resort with panoramic views", 1.249, 103.830),
-                    Place("Botanic Gardens", "One of the world's greatest tropical gardens", 1.3138, 103.8159)
-                )
-            ),
-            userplace("My favorite places in the Midwest",
-                listOf(
-                    Place("Chicago", "Urban center of the midwest, the \"Windy City\"", 41.878, -87.630),
-                    Place("Rochester, Michigan", "The best of Detroit suburbia", 42.681, -83.134),
-                    Place("Mackinaw City", "The entrance into the Upper Peninsula", 45.777, -84.727),
-                    Place("Michigan State University", "Home to the Spartans", 42.701, -84.482),
-                    Place("University of Michigan", "Home to the Wolverines", 42.278, -83.738)
-                )
-            ),
-            userplace("Restaurants to try",
-                listOf(
-                    Place("Champ's Diner", "Retro diner in Brooklyn", 40.709, -73.941),
-                    Place("Althea", "Chicago upscale dining with an amazing view", 41.895, -87.625),
-                    Place("Shizen", "Elegant sushi in San Francisco", 37.768, -122.422),
-                    Place("Citizen Eatery", "Bright cafe in Austin with a pink rabbit", 30.322, -97.739),
-                    Place("Kati Thai", "Authentic Portland Thai food, served with love", 45.505, -122.635)
-                )
-            )
-        )
+
+    //to save data we need to implement three members
+    private fun serializeUserPlaces(context: Context, userplaces: List<userplace>) {
+        Log.i(TAG, "serialize UserPlaces")
+        ObjectOutputStream(FileOutputStream(getDataFile(context))).use { it.writeObject(userplaces) }
+    }
+
+    private fun deserializeUserPlaces(context: Context): List<userplace> {
+        Log.i(TAG, "Deserialize UserPlaces")
+        val datafile = getDataFile(context)
+        if (!datafile.exists()) {
+            Log.i(TAG, "Data does not exist")
+            return emptyList()
+        }
+        ObjectInputStream(FileInputStream(datafile)).use { return it.readObject() as List<userplace> }
+    }
+
+    private fun getDataFile(context: Context): File {
+        Log.i(TAG, "Getting the data from the file!!")
+        return File(context.filesDir, FILE_NAME)
     }
 }
